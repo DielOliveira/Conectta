412919264e39bad2c7dc861d33554b741613bcd8
DielOliveira <diel.faria@gmail.com>
2026-08-04 14:50:30 -0300

    Exibe erros e retorna à lista da agenda

--- app/app/Filament/Resources/Disponibilidades/Pages/CreateDisponibilidade.php
+++ app/app/Filament/Resources/Disponibilidades/Pages/CreateDisponibilidade.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\Disponibilidades\DisponibilidadeResource;
 use App\Services\OrdemServico\OrdemServicoAgendaService;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\ValidationException;
 
 class CreateDisponibilidade extends CreateRecord
 {
@@ -13,6 +14,17 @@ class CreateDisponibilidade extends CreateRecord
 
     protected function handleRecordCreation(array $data): Model
     {
-        return app(OrdemServicoAgendaService::class)->criarDisponibilidade($data['tecnico_id'], $data['data'], $data['hora_inicio'], $data['hora_fim']);
+        try {
+            return app(OrdemServicoAgendaService::class)->criarDisponibilidade($data['tecnico_id'], $data['data'], $data['hora_inicio'], $data['hora_fim']);
+        } catch (ValidationException $exception) {
+            throw ValidationException::withMessages(collect($exception->errors())
+                ->mapWithKeys(fn (array $mensagens, string $campo): array => ["data.{$campo}" => $mensagens])
+                ->all());
+        }
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('index');
     }
 }
--- app/app/Filament/Resources/Disponibilidades/Pages/EditDisponibilidade.php
+++ app/app/Filament/Resources/Disponibilidades/Pages/EditDisponibilidade.php
@@ -7,6 +7,7 @@ use App\Models\OrdemServicoDisponibilidade;
 use App\Services\OrdemServico\OrdemServicoAgendaService;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\ValidationException;
 
 class EditDisponibilidade extends EditRecord
 {
@@ -15,12 +16,23 @@ class EditDisponibilidade extends EditRecord
     protected function handleRecordUpdate(Model $record, array $data): Model
     {
         /** @var OrdemServicoDisponibilidade $record */
-        return app(OrdemServicoAgendaService::class)->atualizarDisponibilidade(
-            $record,
-            (int) $data['tecnico_id'],
-            $data['data'],
-            $data['hora_inicio'],
-            $data['hora_fim'],
-        );
+        try {
+            return app(OrdemServicoAgendaService::class)->atualizarDisponibilidade(
+                $record,
+                (int) $data['tecnico_id'],
+                $data['data'],
+                $data['hora_inicio'],
+                $data['hora_fim'],
+            );
+        } catch (ValidationException $exception) {
+            throw ValidationException::withMessages(collect($exception->errors())
+                ->mapWithKeys(fn (array $mensagens, string $campo): array => ["data.{$campo}" => $mensagens])
+                ->all());
+        }
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return static::getResource()::getUrl('index');
     }
 }
